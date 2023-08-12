1910bf4f34a1cf58f032a5da9dcc5ad2f37e32ec
Kayzeeahh <94433076+kayzeeahh@users.noreply.github.com>
2023-08-12 09:41:26 +0100

    config for lambdalab

---
+++ terraform/lambda.tf
@@ -0,0 +1,21 @@
+
+data "archive_file" "lambda" {
+  type        = "zip"
+  source_file = "../function_code.py"
+  output_path = "lambda_function_payload.zip"
+}
+
+resource "aws_lambda_function" "test_lambda" {
+  # If the file is not in the current working directory you will need to include a
+  # path.module in the filename.
+  filename      = "lambda_function_payload.zip"
+  function_name = "lambda_function_name"
+  role          = aws_iam_role.iam_for_lambda.arn
+  handler       = "index.test"
+
+  source_code_hash = data.archive_file.lambda.output_base64sha256
+
+  runtime = "python3.11"
+
+  
+}
